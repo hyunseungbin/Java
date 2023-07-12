2e1895baf8391c9653cfd980493ab87d543bd0d2
hyunseungbin <79680809+hyunseungbin@users.noreply.github.com>
2023-07-12 10:54:28 +0900

    Update 예산.java

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -2,22 +2,14 @@ import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int check = 0;
         Arrays.sort(d);
         
         for (int i =0; i< d.length;i++){
-            check = d[i];
             if (budget - d[i] >= 0){
                 budget -= d[i];
                 answer++;
             }
         }
-        // j = 0
-        // d.sort()
-        // for i in range(0,len(d)):
-        //     if (budget - d[i]) >= 0:
-        //         budget -= d[i]
-        //         j += 1
         return answer;
     }
 }
