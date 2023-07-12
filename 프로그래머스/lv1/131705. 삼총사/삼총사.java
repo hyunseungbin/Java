2a4ffcf5750b9292eef9f624f783b90db28df5cd
hyunseungbin <79680809+hyunseungbin@users.noreply.github.com>
2023-07-12 14:10:59 +0900

    Update 삼총사.java

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -5,18 +5,13 @@ class Solution {
         while(check <= number.length-3){
             for (int i = check+1; i<number.length-1; i++){
                 for (int j = i + 1; j<number.length; j++){
-                    System.out.printf("check: %d i: %d j:%d ",check,i,j);
-                    System.out.println(number[check] + number[i] + number[j]);
                     if (number[check] + number[i] + number[j] == 0){
                         answer++;
                     }
-
                 }
-
             }
             check++;
         }
-        
         return answer;
     }
 }
