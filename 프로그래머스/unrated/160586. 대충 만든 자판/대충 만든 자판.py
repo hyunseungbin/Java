341a1a0d13a1663acfb2576889487ee7457741bd
seungbeen <79680809+hyunseungbin@users.noreply.github.com>
2023-07-28 17:23:50 +0900

    Update 대충 만든 자판.py

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,20 +1,3 @@
-# def solution(keymap, targets):
-#     answer = []
-    
-#     for target in targets:
-#         result = 0
-#         for tar in target:
-#             check = []
-#             for j in range(len(keymap)):
-#                 if tar in keymap[j]:
-#                     check.append(keymap[j].index(tar) + 1)
-#             if check != []:
-#                 result += min(check)
-#         if result == 0:
-#             answer.append(-1)
-#         else:
-#             answer.append(result)
-#     return answer
 def solution(keymap, targets):
     answer = []
     
@@ -29,7 +12,6 @@ def solution(keymap, targets):
                 result += min(check)
             else:
                 result = -1
-                break  # If the character is not found in any key, break the loop and set result to -1
+                break
         answer.append(result)
-    
     return answer
